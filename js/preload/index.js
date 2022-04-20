51526ef9dcef138d95260707457de364e8cc2165
Jack Mandelkorn <jack@jackmandelkorn.com>
2022-04-20 10:28:12 -0700

    adding loader

--- js/preload/index.js
+++ js/preload/index.js
@@ -22,20 +22,21 @@ const invest = () => {
   const onComplete = () => {
     $(".splash")[0].innerHTML = "<h1>We are <span class='gradient-2'>changing</span> the way people wake up.</h1><br><h3>Your submission has been received! We'll be in touch.</h3>"
   }
-  if (verifyEmail($("#email")[0]) && verifyName($("#name")[0])) {
+  if (verifyName($("#name")[0]) && verifyEmail($("#email")[0])) {
+    $("#submit").addClass("loading")
     $.ajax({
       url: (API + "/invest"),
       type: "PUT",
       data: {
         email: $("#email")[0].value.trim(),
         name: $("#name")[0].value.trim(),
-        name: ($("#equity")[0].value || "").trim(),
+        equity: ($("#equity")[0].value || "").trim(),
       },
       success: (data) => {
         onComplete()
       },
       error: (data) => {
-        console.log(data)
+        console.log("Error")
         onComplete()
       }
     })
