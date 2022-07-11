dbc9f0f92c115050ee05d1651c3df9e9ae7c88ec
Btcminercoin <108886970+Bitcoinstar-1@users.noreply.github.com>
2022-07-11 23:09:55 +0300

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,7 +1,7 @@
 const YEAR = (new Date()).getFullYear()
 const API = "https://0zynwo3qw4.execute-api.us-east-1.amazonaws.com/dev"
 
-console.log("\u00A9 " + YEAR.toString() + " Paywake Corporation")
+console.log("\u00A9 " + YEAR.toString() + " İnvestTurkey Şirketi")
 
 $(document).ready(() => {
   try {
